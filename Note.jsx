285b6a3a289ea10fd3312863f51fc62ae57f4d4b
DeyNik <85392250+DeyNik@users.noreply.github.com>
2021-06-05 15:32:35 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Art is..</h1>
+      <p>
+        Art is my escapism..it captivates my mind, my heart and teleports me to
+        a whole new world! Becoming my solace!! It's my magic that lets me
+        create some.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
